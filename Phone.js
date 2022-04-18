aca732f5005b35fc58d2b133d4b2eab6d7fa5b86
Antoniyski <antoniyski@gmail.com>
2022-04-18 09:14:51 +0300

    Testing objects

---
+++ Phone.js
@@ -0,0 +1,15 @@
+class Phone {
+    constructor(
+        model,
+        color
+    ) {
+        this.phoneModel = model;
+        phoneColor = color;
+    }
+
+    setPhoneCOlor(color) {
+        this.phoneColor = color;
+    }
+}
+
+export default Phone;
---
+++ script.js
@@ -0,0 +1,14 @@
+/**
+ * Practice: Making classes and objects
+ *
+ * - Find a type of object you have more than one of in your house (eg. clothing, writing tools, etc).
+ * - Create a class describing this object type - its properties and methods.
+ * - Create several objects using the class.
+ * - Test the objecs by calling their properties and using their methods in the console.
+ */
+
+import Phone from "./Phone.js";
+
+const blackSamsung = new Phone("Samsung", "black");
+const redSamsung = new Phone("Samsung", "red");
+const whiteApple = new Phone("Apple", "white")
